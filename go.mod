18c4ed3fd52f9e98d58a91577ca7e02be920cf9a
kavorite <kavorite@gmail.com>
2024-06-07 16:37:47 -0400

    init

---
+++ go.mod
@@ -0,0 +1,23 @@
+module github.com/kavorite/spaider
+
+go 1.22.4
+
+require (
+	github.com/JohannesKaufmann/html-to-markdown v1.6.0 // indirect
+	github.com/PuerkitoBio/goquery v1.9.2 // indirect
+	github.com/andybalholm/cascadia v1.3.2 // indirect
+	github.com/antchfx/htmlquery v1.2.3 // indirect
+	github.com/antchfx/xmlquery v1.2.4 // indirect
+	github.com/antchfx/xpath v1.1.8 // indirect
+	github.com/gobwas/glob v0.2.3 // indirect
+	github.com/gocolly/colly/v2 v2.1.0 // indirect
+	github.com/golang/groupcache v0.0.0-20200121045136-8c9f03a8e57e // indirect
+	github.com/golang/protobuf v1.4.2 // indirect
+	github.com/kennygrant/sanitize v1.2.4 // indirect
+	github.com/saintfish/chardet v0.0.0-20120816061221-3af4cd4741ca // indirect
+	github.com/temoto/robotstxt v1.1.1 // indirect
+	golang.org/x/net v0.26.0 // indirect
+	golang.org/x/text v0.16.0 // indirect
+	google.golang.org/appengine v1.6.6 // indirect
+	google.golang.org/protobuf v1.24.0 // indirect
+)
---
+++ main.go
@@ -0,0 +1,110 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"log"
+	"net/url"
+	"os"
+	"strings"
+
+	md "github.com/JohannesKaufmann/html-to-markdown"
+	"github.com/gocolly/colly/v2"
+)
+
+// Extensions to filter
+type extensions []string
+
+func (e *extensions) String() string {
+	return fmt.Sprint(*e)
+}
+
+func (e *extensions) Set(value string) error {
+	*e = append(*e, value)
+	return nil
+}
+
+var (
+	converter     = md.NewConverter("", true, nil)
+	startPath     string
+	startURL      string
+	allowedDomain string
+	allowedExts   extensions
+	defaultExts   = extensions{"", ".html", ".md", ".txt", ".rst"}
+)
+
+func main() {
+	flag.StringVar(&startURL, "url", "", "The root URL to start scraping from")
+	flag.Var(&allowedExts, "ext", `File extensions to allow (default: "", ".html", ".md", ".txt", ".rst")`)
+	flag.Parse()
+
+	if startURL == "" {
+		log.Fatal("Please provide a URL to start scraping from using the -url flag.")
+	}
+
+	// Use default extensions if none provided
+	if len(allowedExts) == 0 {
+		allowedExts = defaultExts
+	}
+
+	parsedURL, err := url.Parse(startURL)
+	if err != nil {
+		log.Fatalf("Failed to parse URL: %v", err)
+	}
+	startPath = parsedURL.Path
+	allowedDomain = parsedURL.Host
+
+	c := colly.NewCollector(
+		colly.AllowedDomains(allowedDomain),
+		colly.UserAgent("github.com/kavorite/spaider"),
+		colly.MaxBodySize(1<<19), // according to the HTTP Archive, 99% of text documents should be under this limit
+	)
+
+	c.OnRequest(func(r *colly.Request) {
+		fmt.Fprintf(os.Stderr, "%s:\n", r.URL.String())
+	})
+
+	c.OnResponse(func(r *colly.Response) {
+		path := r.Request.URL.Path
+		mime := r.Headers.Get("Content-Type")
+		if !isAllowedExtension(path) || !isAllowedContentType(mime) || !strings.HasPrefix(path, startPath) {
+			return
+		}
+		fmt.Printf("%s:\n", r.Request.URL.String())
+		text := string(r.Body)
+
+		if strings.HasSuffix(r.Request.URL.Path, ".html") || strings.Contains(mime, "html") {
+			text = toMarkdown(text)
+		}
+
+		fmt.Println(text)
+	})
+
+	c.OnHTML("a[href]", func(e *colly.HTMLElement) {
+		link := e.Attr("href")
+		if _, err := url.Parse(link); err == nil {
+			e.Request.Visit(e.Request.AbsoluteURL(link))
+		}
+	})
+
+	c.Visit(startURL)
+	c.Wait()
+}
+
+func isAllowedExtension(path string) bool {
+	for _, ext := range allowedExts {
+		if strings.HasSuffix(path, ext) {
+			return true
+		}
+	}
+	return false
+}
+
+func isAllowedContentType(mime string) bool {
+	return strings.HasPrefix(mime, "text/")
+}
+
+func toMarkdown(html string) string {
+	mkdn, _ := converter.ConvertString(html)
+	return mkdn
+}
